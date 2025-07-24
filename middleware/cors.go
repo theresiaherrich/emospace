b606e7ec99f70f097c1290c04cbc8f382a187f10
theresiaherrich <124873369+theresiaherrich@users.noreply.github.com>
2025-07-24 10:46:34 +0700

    fix: cors

--- middleware/cors.go
+++ middleware/cors.go
@@ -1,21 +1,23 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+    "github.com/gin-gonic/gin"
+)
 
 func CORSMiddleware() gin.HandlerFunc {
     return func(c *gin.Context) {
         origin := c.Request.Header.Get("Origin")
 
         allowedOrigins := map[string]bool{
-            "http://localhost:5173":        true, 
-            "https://emospaces.bccdev.id": true, 
+            "http://localhost:5173":        true,
+            "https://emospaces.bccdev.id":  true,
         }
 
         if allowedOrigins[origin] {
             c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
             c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
             c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-            c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+            c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
         }
 
         if c.Request.Method == "OPTIONS" {
@@ -26,3 +28,4 @@ func CORSMiddleware() gin.HandlerFunc {
         c.Next()
     }
 }
+
