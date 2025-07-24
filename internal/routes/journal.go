c83c77fe3efdb3267f54252b9409581a05937057
theresiaherrich <124873369+theresiaherrich@users.noreply.github.com>
2025-07-24 19:13:28 +0700

    fix: route journals

--- internal/routes/journal.go
+++ internal/routes/journal.go
@@ -2,15 +2,17 @@ package routes
 
 import (
 	"emospaces-backend/internal/handler"
+	"emospaces-backend/middleware"
+
 	"github.com/gin-gonic/gin"
 )
 
 func RegisterJournalRoutes(r *gin.RouterGroup, h *handler.JournalHandler) {
 	journal := r.Group("/journals")
+	journal.Use(middleware.JWTMiddleware())
 	{
 		journal.POST("/", h.CreateJournal)
 		journal.GET("/", h.GetJournals)
-		journal.GET("/me", h.GetMyJournals)
 		journal.GET("/:id", h.GetJournalDetail)
 		journal.PUT("/:id", h.UpdateJournal)
 		journal.DELETE("/:id", h.DeleteJournal)
