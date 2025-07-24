17cb4655c08ed6b879c328264bb300da60810e26
theresiaherrich <124873369+theresiaherrich@users.noreply.github.com>
2025-07-24 10:41:06 +0700

    fix: model chat

--- internal/models/chat.go
+++ internal/models/chat.go
@@ -7,7 +7,7 @@ type ChatLog struct {
 	UserID    uint      `gorm:"index"`
 	UserName  string    `gorm:"index"`
 	Mood      string
-	UserInput string
-	AIOutput  string
+	UserInput string    `gorm:"type:text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci"`
+	AIOutput  string    `gorm:"type:text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci"`
 	CreatedAt time.Time
 }
